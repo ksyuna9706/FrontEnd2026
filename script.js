bedb79898feeabfe00495a2766c6163e2acbab77
ksyuna_97 <ksyuna_97@mail.ru>
2026-06-07 16:55:34 +0300

    object

--- script.js
+++ script.js
@@ -4399,5 +4399,91 @@ function имя функции(аргументы){
 // // document.writeln(re);
 // console.log(re);
 // console.log("Вносим изменения");
-console.log("Вносим изменения на другом рабочем месте");
+// console.log("Вносим изменения на другом рабочем месте");
+// let registrationForm = document.getElementById("registrationForm");
+// let errorMessages = document.getElementById("errorMessages");
+// registrationForm.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     let [username, email, password] = registrationForm.elements;
+//     errorMessages.innerHTML = "";
+//     //  console.log(username,email,password);
+//     if (!username.value.trim()) {
+//         displayError("Имя пользователя обязательно!");
+//         return;
+//     }
+//     if (!email.value.trim()|| !isValidEmail(email.value)) {
+//         displayError("Введите адрес электронной почты!");
+//         return;
+//     }
+//     if(!password.value.trim() ||!isStrongPassword(password.value)){
+//         displayError("Пароль должен состоять как минимум из 8 символов и как минимум одну заглавную букву,одну строчную букву,одну цифру и один спец.символ");
+//     }
+//     alert("Register successfull!");
+//     registrationForm.reset();
+// });
+// function displayError(message) {
+//     errorMessages.innerHTML += `<div class="error">${message}</div>`
+// }
+// function isValidEmail(email){
+// return /^[\w.%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i.test(email);
+// }
+// function isStrongPassword(password){
+//     return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/.test(password);
+// }
+            //ОБЪЕКТЫ
+// let car = new Object();
+// let car1 = {};
+// console.log(car);
+// console.log(car1);
+// let car = new Object();
+// car["type"] = "BMW";
+// car["color"] = "white";
+// console.log(car);
+// // alert(car);
+// document.writeln(car["type"] + "" + car["color"]+"<br>");
+// let car1 = new Object();
+// car1.type = "BMW";
+// car1.color = "black";
+// document.writeln(car1.type + "" + car1.color);
 
+// let menu1 = {};
+// menu1.width = 300;
+// menu1.height = 200;
+// menu1.title = "Menu";
+// console.log(menu1);
+// document.writeln(menu1.title + ":" + menu1.width + "x" + menu1.height + "<br>");
+// let menu = {
+//     width: 300,
+//     height:200,
+//     title:"Menu"
+// };
+
+// delete menu.width;
+// document.writeln(menu.title + ":" + menu.width + "x" + menu.height + "<br>");
+// console.log("width" in menu);
+// console.log("height" in menu);
+
+// let counter=0;
+// for(let key in menu){
+//     document.writeln("<br>Ключ ="+key+",значение="+menu[key]);
+//     counter++;
+// }
+// document.writeln("<br><br>Всего свойств :"+counter+"<br>");
+// document.writeln("<br><br>Всего свойств :"+Object.keys(menu)+"<br>");
+// document.writeln("<br><br>Всего свойств :"+Object.keys(menu).length+"<br>");
+// document.writeln(Object.keys(menu));
+// Object.keys(menu).forEach(function(key){
+//     document.writeln("<br>"+key+" " +menu[key]);
+// })
+// Object.values(menu).forEach(function(v){
+//     document.writeln("<br>"+v);
+// })
+// console.log(Object.values(menu));
+// console.log(Object.entries(menu));
+
+// for(let el of Object.entries(menu)){
+//     console.log(el[0],el[1]);
+//     document.writeln("<br>"+el)
+// }
+
+//Крестики нолики
