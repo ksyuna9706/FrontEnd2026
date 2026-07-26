a6f779f3475d226c345934725a8dbab4490fc806
ksyuna_97 <ksyuna_97@mail.ru>
2026-07-26 16:40:23 +0300

    game snake 260726

---
+++ snake/script.js
@@ -0,0 +1,295 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const scoreEl = document.getElementById("score");
+const startButton = document.getElementById("startButton");
+const gridSize = 20;//размер одного сегмента/клетки
+let snake, direction,foot,score,gameSpeed,gameRunning;//змейка,направление
+
+function startGame() {
+    snake = [{//координаты змейки
+        x: 200,
+        y: 200
+    }];
+
+    direction = "RIGHT";//направление движения по умолчанию
+    score = 0;
+    gameSpeed = 150;//скорость игры
+    foot = getRandomFootPosition();//появление квадрата для съедения змейкой
+    gameRunning = true;
+
+    scoreEl.textContent = "Счет: 0";
+    startButton.style.display = "none";
+    gameLoop();//начало  игры
+}
+
+
+function getRandomFootPosition(){
+    return {
+        x: Math.floor(Math.random()*(canvas.width/gridSize))*gridSize,
+        y: Math.floor(Math.random()*(canvas.height/gridSize))*gridSize,
+    }
+}
+
+//функция конца игры
+function gameOver() {
+    gameRunning = false;
+    let result = document.createElement("p");
+    result.innerHTML = `<span style='color:red'>Игра окончена!</span>`;
+    scoreEl.prepend(result);
+    startButton.style.display = 'inline';
+}
+
+//анимация змейки
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "purple";
+        ctx.fillRect(foot.x, foot.y, gridSize, gridSize);
+        ctx.fillStyle = "black";
+        ctx.strokeRect(foot.x, foot.y, gridSize, gridSize);
+
+    snake.forEach(segment => {
+        ctx.fillStyle = "blue";
+        ctx.fillRect(segment.x, segment.y, gridSize, gridSize);
+        ctx.fillStyle = "black";
+        ctx.strokeRect(segment.x, segment.y, gridSize, gridSize);
+    });
+
+    let head = { ...snake[0] };//перемещение змеи вверх
+    if (direction == "UP") {
+        head.y -= gridSize;
+    }
+    if (direction == "DOWN") {
+        head.y += gridSize;
+    }
+    if (direction == "LEFT") {
+        head.x -= gridSize;
+    }  
+    if (direction == "RIGHT") {
+        head.x += gridSize;
+    }
+
+    if (head.x < 0 || head.x >=canvas.width || head.y >=canvas.height || head.y < 0){
+        gameOver();
+        return;
+    }
+     for (let i = 0; i < snake.length; i++) {
+        if(head.x === snake[i].x && head.y === snake[i].y){
+            gameOver();
+            return;
+        }
+        
+     }
+
+    snake.unshift(head);
+
+if(head.x === foot.x && head.y === foot.y){
+    foot = getRandomFootPosition();
+    score++;
+    scoreEl.textContent = `Счет: ${score}`;
+if(gameSpeed>50){
+    gameSpeed-=5;
+}
+}else{
+snake.pop();//убрали хвост
+}
+
+
+    if (snake.length > 0) {
+        setTimeout(gameLoop, gameSpeed);
+    }
+}
+
+function changeDirection(event) {
+    const key = event.key;
+    if (key == "ArrowUp" && direction !== "DOWN") {//ArrowUp-зарезервированное название
+        direction = "UP";
+    } else if (key == "ArrowDown" && direction !== "UP") {
+        direction = "DOWN";
+
+    } else if (key == "ArrowLeft" && direction !== "RIGHT") {
+        direction = "LEFT";
+
+    } else if (key == "ArrowRight" && direction !== "LEFT") {
+        direction = "RIGHT";
+    }
+    // console.log(direction);
+
+}
+document.addEventListener("keydown", changeDirection)
+startButton.addEventListener("click", startGame);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Game {
+//     constructor(element, gameBoard, size) {
+//         this.element = element;
+//         this.width = gameBoard;
+//         this.height = gameBoard;
+//         // this.width = this.height = gameBoard;
+//         this.gridSize = size;
+ 
+//         this.canvas = document.createElement("canvas");
+//         this.canvas.width = this.width;
+//         this.canvas.height = this.height;
+//         this.ctx = this.canvas.getContext('2d');
+ 
+//         this.element.append(this.canvas);
+ 
+//         this.scoreEl = document.getElementById("score");
+//         this.startButton = document.getElementById("startButton");
+ 
+//         this.snake = [{
+//             x: this.canvas.width / 2,
+//             y: this.canvas.height / 2
+//         }];
+ 
+//         this.direction = "RIGHT";
+//         this.gameSpeed = 200;
+//         this.foot = {};
+//         this.score = 0;
+ 
+//         this.gameRunning = false;
+//     }
+ 
+//     startGame() {
+//         this.foot = this.getRandomFootPosition();
+//         this.gameRunning = true;  
+ 
+ 
+//         this.scoreEl.textContent = "Счет: 0";
+//         this.startButton.style.display = "none";
+ 
+//         this.gameLoop();
+//     }
+ 
+//     getRandomFootPosition() {
+//         return {
+//             x: Math.floor(Math.random() * (this.canvas.width / this.gridSize)) * this.gridSize,
+//             y: Math.floor(Math.random() * (this.canvas.height / this.gridSize)) * this.gridSize
+//         }
+//     }
+ 
+//     gameLoop() {
+//         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+ 
+//         this.ctx.fillStyle = "purple";
+//         this.ctx.fillRect(this.foot.x, this.foot.y, this.gridSize, this.gridSize);
+//         this.ctx.fillStyle = "black";
+//         this.ctx.strokeRect(this.foot.x, this.foot.y, this.gridSize, this.gridSize);
+ 
+//         this.snake.forEach(segment => {
+//             this.ctx.fillStyle = "blue";
+//             this.ctx.fillRect(segment.x, segment.y, this.gridSize, this.gridSize);
+//             this.ctx.fillStyle = "black";
+//             this.ctx.strokeRect(segment.x, segment.y, this.gridSize, this.gridSize);
+//         });
+ 
+//         let head = { ...this.snake[0] };
+ 
+//         if (this.direction === "UP") {
+//             head.y -= this.gridSize;
+//         }
+//         if (this.direction === "DOWN") {
+//             head.y += this.gridSize;
+//         }
+//         if (this.direction === "LEFT") {
+//             head.x -= this.gridSize;
+//         }
+//         if (this.direction === "RIGHT") {
+//             head.x += this.gridSize;
+//         }
+ 
+//         if (head.x < 0 || head.x >= this.canvas.width || head.y >= this.canvas.height || head.y < 0) {
+//             this.gameOver();
+//             return;
+//         }
+ 
+//         for (let i = 1; i < this.snake.length; i++) {
+//             if (head.x === this.snake[i].x && head.y === this.snake[i].y) {
+//                 this.gameOver();
+//                 return;
+//             }
+//         }
+ 
+//         this.snake.unshift(head);
+ 
+//         if (head.x === this.foot.x && head.y === this.foot.y) {
+//             this.foot = this.getRandomFootPosition();
+//             this.score++;
+//             this.scoreEl.textContent = `Счет: ${this.score}`;
+//             if (this.gameSpeed > 50) {
+//                 this.gameSpeed -= 5;
+//             }
+//         } else {
+//             this.snake.pop();
+//         }
+ 
+//         if (this.snake.length > 0) {
+//             setTimeout(() => this.gameLoop(), this.gameSpeed);
+//         }
+ 
+//         // console.log(this.snake);
+//     }
+ 
+//     changeDirection(event) {
+//         const key = event.key;
+//         // console.log(key);
+ 
+ 
+//         if (key == "ArrowUp" && this.direction !== "DOWN") {
+//             this.direction = "UP";
+//         } else if (key == "ArrowDown" && this.direction !== "UP") {
+//             this.direction = "DOWN";
+//         } else if (key == "ArrowLeft" && this.direction !== "RIGHT") {
+//             this.direction = "LEFT";
+//         } else if (key == "ArrowRight" && this.direction !== "LEFT") {
+//             this.direction = "RIGHT";
+//         }
+ 
+//         // console.log(this.direction);
+//     }
+ 
+//     gameOver() {
+//         this.gameRunning = false;
+//         let result = document.createElement("p");
+//         result.innerHTML = `<span style="color: red">Игра окончена! Ваш счет: ${this.score}</span>`;
+//         this.scoreEl.prepend(result);
+//         this.startButton.style.display = "inline";
+//     }
+// }
+ 
+// const root = document.getElementById("root");
+// const game = new Game(root, 400, 20);
+// document.addEventListener("keydown", (event) => game.changeDirection(event));
+// game.startButton.addEventListener("click", () => game.startGame());
+ 
+
+
+
--- tetris/script.js
+++ tetris/script.js
@@ -1,29 +1,265 @@
 class Game {
-    playfield = [
-        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ]
-}
 
+    static points = {
+        "1": 40,
+        "2": 100,
+        "3": 300,
+        "4": 1200,
+    };
+    score = 0;
+    lines = 0;
+
+    playfield = this.createPlayfield();
+
+    activePiece = this.createPiece();
+    nextPiece = this.createPiece();
+
+    getLevel() {
+        return Math.ceil(this.lines * 0.1);
+    }
+
+    getState() {
+        const playfield = this.createPlayfield();
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+        for (let y = 0; y < this.playfield.length; y++) {
+            playfield[y] = [];
+
+            for (let x = 0; x < this.playfield[y].length; x++) {
+                playfield[y][x] = this.playfield[y][x];
+            }
+        }
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (blocks[y][x]) {
+                    playfield[pieceY + y][pieceX + x] = blocks[y][x];
+                }
+            }
+        }
+
+        return {
+            playfield
+        }
+    }
+
+    moveIsLeft() {
+        this.activePiece.x--;
+
+        if (this.hasCollision()) {
+            this.activePiece.x++;
+        }
+    }
+
+    moveIsRight() {
+        this.activePiece.x++;
+
+        if (this.hasCollision()) {
+            this.activePiece.x--;
+        }
+    }
+
+    moveIsDown() {
+        this.activePiece.y++;
+
+        if (this.hasCollision()) {
+            this.activePiece.y--;
+            this.lockPiece();
+            const clearLines = this.clearLines();
+            this.updateScore(clearLines);
+            this.updatePieces();
+        }
+    }
+    updateScore(clearedLines) {
+        if (clearedLines > 0) {
+            this.score += Game.points[clearedLines]*(this.getLevel()+1);
+            this.lines += clearedLines;
+            console.log(this.score, this.lines,this.getLevel());
+
+        }
+    }
+
+    clearLines() {
+        const rows = 20;
+        const columns = 10;
+        let lines = [];
+        for (let y = rows - 1; y >= 0; y--) {
+            let numberOfblocks = 0;
+            for (let x = 0; x < columns; x++) {
+                if (this.playfield[y][x]) {
+                    numberOfblocks++;
+                }
+
+            }
+            if (numberOfblocks === 0) {
+                break;
+            } else if (numberOfblocks < columns) {
+                continue;
+            } else {//numberOfblocks < columns
+                lines.unshift(y);
+            }
+        }
+        for (let index of lines) {
+            this.playfield.splice(index, 1);
+            this.playfield.unshift(new Array(columns).fill(0));
+        }
+        return lines.length;
+    }
+
+    hasCollision() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (blocks[y][x] && ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined || this.playfield[pieceY + y][pieceX + x]))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    lockPiece() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (blocks[y][x]) {
+                    this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+                }
+            }
+        }
+    }
+
+    createPlayfield() {
+        const plyfield = [];  // []
+
+        for (let y = 0; y < 20; y++) {
+            plyfield[y] = [];  // [[], [], [], ...]
+
+            for (let x = 0; x < 10; x++) {
+                plyfield[y][x] = 0; // [[0, 0, 0, ...], [0], [0], ...]                
+            }
+        }
+        return plyfield;
+    }
+
+    rotationPiece() {
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
+
+        const temp = [];
+        for (let i = 0; i < length; i++) {
+            temp[i] = new Array(length).fill(0); // [[0,0,0], [0,0,0], [0,0,0]]            
+        }
+
+        for (let y = 0; y < length; y++) {
+            for (let x = 0; x < length; x++) {
+                temp[x][y] = blocks[length - 1 - y][x];
+            }
+        }
+
+        this.activePiece.blocks = temp;
+
+        if (this.hasCollision()) {
+            this.activePiece.blocks = blocks;
+        }
+    }
+
+    updatePieces() {
+        this.activePiece = this.nextPiece;
+        this.nextPiece = this.createPiece();
+    }
+
+    createPiece() {
+
+        const index = Math.floor(Math.random() * 7);
+        const type = "IJLOSTZ"[index];
+        const piece = {};
+        switch (type) {
+            case "I":
+                piece.blocks = [
+                    [0, 0, 0, 0],
+                    [1, 1, 1, 1],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0]
+                ];
+                break;
+            case "J":
+                piece.blocks = [
+                    [0, 0, 0],
+                    [2, 2, 2],
+                    [2, 0, 0],
+
+                ];
+                break;
+            case "L":
+                piece.blocks = [
+                    [0, 0, 0],
+                    [3, 3, 3],
+                    [0, 0, 3],
+
+                ];
+                break;
+            case "O":
+                piece.blocks = [
+                    [0, 0, 0, 0],
+                    [0, 4, 4, 0],
+                    [0, 4, 4, 0],
+                    [0, 0, 0, 0]
+                ];
+                break;
+            case "S":
+                piece.blocks = [
+                    [0, 0, 0],
+                    [0, 5, 5],
+                    [5, 5, 0],
+
+                ];
+                break;
+            case "T":
+                piece.blocks = [
+                    [0, 0, 0],
+                    [6, 6, 6],
+                    [0, 6, 0],
+                ];
+                break;
+            case "Z":
+                piece.blocks = [
+                    [0, 0, 0],
+                    [7, 7, 0],
+                    [0, 7, 7],
+                ];
+                break;
+            default:
+                throw new Error("Неизвестный тип фигуры");
+        }
+        piece.x = Math.floor((10 - piece.blocks[0].length) / 2)
+        piece.y = -1;
+        return piece;
+        // x: 0,
+        // y: 0,
+        // get blocks(){
+        //     return this.rotation[this.rotationIndex];
+        // },
+        // blocks: [
+        //     [0, 1, 0],
+        //     [1, 1, 1],
+        //     [0, 0, 0]
+        // ]
+
+    }
+}
 class View {
+    static colors = {
+        "1": "cyan",
+        "2": "blue",
+        "3": "orange",
+        "4": "yellow",
+        "5": "green",
+        "6": "purple",
+        "7": "red"
+    }
     constructor(element, width, height, rows, columns) {
         this.element = element;
         this.width = width;
@@ -37,26 +273,81 @@ class View {
         this.element.append(this.canvas);
 
     }
+
+    clearScreen() {
+        this.context.clearRect(0, 0, this.width, this.height);
+
+    }
+    render({ playfield }) {
+        this.clearScreen()
+        this.renderPlayfield(playfield);
+    }
     renderPlayfield(playfield) {
         for (let y = 0; y < playfield.length; y++) {
             const line = playfield[y];
             for (let x = 0; x < line.length; x++) {
                 const block = line[x];
                 if (block) {
-                    this.context.fillStyle = "red";
+                    this.context.fillStyle = View.colors[block];
                     this.context.strokeStyle = "black";
                     this.context.lineWidth = 2;
-                    this.context.fillRect(x *this.blockWidth,y*this.blockHeight,this.blockWidth,this.blockHeight);
-                    this.context.strokeRect(x *this.blockWidth,y*this.blockHeight,this.blockWidth,this.blockHeight);
+                    this.context.fillRect(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight);
+                    this.context.strokeRect(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight);
                 }
 
             }
 
         }
     }
+
+
+    updatePieces() {
+        this.activePiece = this.nextPiece;
+        this.nextPiece = this.createPiece();
+    }
+
+    createPiece() {
+        return {
+            x: 0,
+            y: 0,
+            get blocks() {
+                return this.rotation[this.rotationIndex];
+            },
+            blocks: [
+                [0, 1, 0],
+                [1, 1, 1],
+                [0, 0, 0]
+            ]
+        }
+    }
 }
+
 const game = new Game();
 
 const root = document.querySelector("#root");
 const view = new View(root, 320, 640, 20, 10);
-view.renderPlayfield(game.playfield);
+// view.renderPlayfield(game.playfield);
+// console.log(game.playfield);
+document.addEventListener("keydown", event => {
+    switch (event.which) {//keyCode
+        case 37:
+            game.moveIsLeft();
+            view.render(game.getState());
+            break;
+        case 38:
+            game.rotationPiece();
+            view.render(game.getState());
+            break;
+        case 39:
+            game.moveIsRight();
+            view.render(game.getState());
+            break;
+        case 40:
+            game.moveIsDown();
+            view.render(game.getState());
+            break;
+
+
+    }
+})
+
